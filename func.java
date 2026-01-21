bee3fe5167b1776377e1c7dc9a95fff8f607c148
IamSamk <kadam.samartha@yahoo.com>
2026-01-21 11:37:02 +0530

    operations

---
+++ func.java
@@ -0,0 +1,9 @@
+public class func {
+    public static void add(int a,int b){
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
+    }
+    public static void main(String[] args) {
+        add(6, 7);
+    }
+}
