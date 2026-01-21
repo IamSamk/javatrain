bc90f8d19e2425ec377ef618374ba7921578aafb
IamSamk <kadam.samartha@yahoo.com>
2026-01-21 11:06:31 +0530

    input?

---
+++ demo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Arrays;
+class Numarray{
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int[] numbers = new int[10];
+    for(int i=0;i<10;i++){
+	   numbers[i] = scanner.nextInt();
+        }
+    for(int i=0;i<10;i++){
+	  System.out.println(numbers[i]);
+        }
+    }
+
+}
