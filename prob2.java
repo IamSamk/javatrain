fa5d0931747e4f0ef723e031824c4237d78255bd
IamSamk <kadam.samartha@yahoo.com>
2026-02-02 10:59:12 +0530

    patch

---
+++ prob2.java
@@ -0,0 +1,8 @@
+public class prob2{
+    public static void main(String[] args) {
+        int x = 15;
+        for (int i = 10; i <= x; i++) {
+            System.out.println(i);
+        }
+    }
+}
