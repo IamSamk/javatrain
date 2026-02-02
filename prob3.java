e948d6d05d89f5cf5a42d5be4086a3502b25b619
IamSamk <kadam.samartha@yahoo.com>
2026-02-02 10:59:30 +0530

    patch

---
+++ prob3.java
@@ -0,0 +1,8 @@
+public class prob3{
+    public static void main(String[] args) {
+    int x = 6;
+    int y = 7;
+    int z = 9;
+    System.out.println("The sum of the three numbers is: " + (x+y+z));
+    }
+}
