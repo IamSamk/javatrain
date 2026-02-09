a67bca3d2559f84dac45b8ed5552fe82567df4f0
IamSamk <kadam.samartha@yahoo.com>
2026-02-09 10:06:51 +0530

    solving

---
+++ day2_4.java
@@ -0,0 +1,11 @@
+public class day2_4 {
+    public static void main(String[] args) {
+        int a =2;
+        int b =3;
+        System.out.println("Before swapping: a = "+a+" b = "+b);
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println("After swapping: a = "+a+" b = "+b);
+    }
+}
