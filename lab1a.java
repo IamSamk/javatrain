de0e0945b1ce58b2995be20a86a382cd256982b5
IamSamk <kadam.samartha@yahoo.com>
2026-02-16 10:02:56 +0530

    lab programs

---
+++ lab1a.java
@@ -0,0 +1,12 @@
+class lab1a{
+    public static void main(String[] args) {
+        String name = "Samarth Kadam";
+        String rollno = "1RUA24CSE0399";
+        String dept = "BTech, Computer Science and engineering";
+        String college = "RVU";
+        System.out.println("Name :" + name);
+        System.out.println("Roll Number :" + rollno);
+        System.out.println("Department :" + dept);
+        System.out.println("College :" + college);
+    }
+}
